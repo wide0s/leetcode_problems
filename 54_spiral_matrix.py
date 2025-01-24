f2b125d8e162777ffcd38a5d59d30c1c40a09253
wide0s <vladimir.sergeev@gmail.com>
2025-01-25 00:19:33 +0300

    Corrected description for the problem 54

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -1,9 +1,11 @@
-# You are given an n x n 2D matrix representing an image,
-# rotate the image by 90 degrees (clockwise).
-
-# You have to rotate the image in-place, which means you
-# have to modify the input 2D matrix directly. DO NOT allocate
-# another 2D matrix and do the rotation.
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+#
+# Input = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# Output = [1,2,3,6,9,8,7,4,5]
 
 class Solution(object):
     def spiralOrder(self, matrix):
